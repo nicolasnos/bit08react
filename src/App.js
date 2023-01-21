b9f20603b0cc55e6e9f5b14a8b39efad19b1b120
Nicolas Olmos <nicolas.nos@hotmail.com>
2023-01-20 21:45:58 -0500

    bootstrap agregado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
+import Button from "react-bootstrap/Button";
 import "./App.css";
 
 function App() {
-  return <div className="App">holi</div>;
+  return (
+    <div className="App">
+      <p>holi</p>
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
 }
 
 export default App;
